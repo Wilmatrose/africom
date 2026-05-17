4848e13623fca949010525b7018b2e02af0fcf35
Wilmatrose <okolid52@gmail.com>
2026-05-17 16:28:52 -0700

    Enable DB sync to create tables

--- src/modules/groups/groups.module.ts
+++ src/modules/groups/groups.module.ts
@@ -1,14 +1,17 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
-import { EventEmitterModule } from '@nestjs/event-emitter'; // Import EventEmitterModule
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
-import { GroupsGateway } from './groups.gateway'; // Import Gateway
+import { GroupsGateway } from './groups.gateway';
+
+// Entity Imports
 import { Group } from './entities/group.entity';
 import { GroupMember } from './entities/group-member.entity';
 import { GroupMessage } from './entities/group-message.entity';
+import { GroupMessageReaction } from './entities/group-message-reaction.entity'; // <--- ADDED IMPORT
 import { Notification } from '../notifications/notification.entity';
 
 @Module({
@@ -17,13 +20,14 @@ import { Notification } from '../notifications/notification.entity';
       Group, 
       GroupMember,
       GroupMessage,
+      GroupMessageReaction, // <--- ADDED ENTITY
       Notification,
     ]),
     forwardRef(() => AuthModule),
-    EventEmitterModule.forRoot(), // Enable Event Emitter
+    EventEmitterModule.forRoot(),
   ],
   controllers: [GroupsController],
-  providers: [GroupsService, GroupsGateway], // Add Gateway to providers
+  providers: [GroupsService, GroupsGateway],
   exports: [GroupsService],
 })
 export class GroupsModule {}
